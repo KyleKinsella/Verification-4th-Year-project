240e954a9b7e1cff6cc369977b76f6e08d3a5196
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-22 11:21:29 +0100

    I have added more tests

--- Task1/MainTest.java
+++ Task1/MainTest.java
@@ -87,4 +87,66 @@ class MainTest {
 
         assertEquals(period.duration(), 6, e.getMessage());
     }
+
+
+    @Test
+    void startHourNotEqualDuration() {
+        int start = 3;
+        int end = 2;
+
+        Period period = new Period(start, end);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            period.duration();
+        });
+
+        assertEquals(period.duration(), 2, e.getMessage());
+    }
+
+
+    @Test
+    void endHourNotEqualDuration() {
+        int start = 2;
+        int end = 3;
+
+        Period period = new Period(start, end);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            period.duration();
+        });
+
+        assertEquals(period.duration(), 1, e.getMessage());
+    }
+    // end of test's for duration method
+
+
+
+
+    // test's for Overlaps(Period period) method
+    @Test
+    void validOverlapsMethod() {
+        Period period1 = new Period(1, 2);
+        Period period2 = new Period(2, 4);
+
+        assertEquals(period1.overlaps(period2), true);
+    }
+
+    @Test
+    void invalidOverlapsMethod() {
+        Period period1 = new Period(1, 2);
+        Period period2 = new Period(10, 12);
+
+        assertEquals(period1.overlaps(period2), false);
+    }
+
+    @Test
+    void periodOneEqualToPeriodTwo() {
+        Period period1 = new Period(1, 2);
+        Period period2 = new Period(1, 2);
+
+        assertEquals(period1.equals(period2), true);
+    }
+
+    // end of test's for Overlaps(Period period) method
+
 }
