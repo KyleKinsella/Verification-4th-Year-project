6a42c7bd4846e6fa4597e92190904171c127058e
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-23 15:53:24 +0100

    I have added some tests for the overlaps method, some more tests for the period constructor and I have also added some tests for my Rate class

--- Task1/MainTest.java
+++ Task1/MainTest.java
@@ -1,3 +1,5 @@
+//package cm;
+
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -26,23 +28,6 @@ class MainTest {
 
         assertNotNull(period);
     }
-    // end of test's for Period constructor
-
-
-
-
-    // test's for duration method
-    @Test
-    void validStartAndEndPeriod() {
-
-        int start = 4;
-        int end = 6;
-
-        Period p1 = new Period(start, end);
-
-        assertEquals(p1.duration(), 2);
-    }
-
 
     @Test
     void checkSameTime() {
@@ -55,8 +40,48 @@ class MainTest {
     }
 
     @Test
+    void boundaryValuesInvalidObject() {
+        int start = -2;
+        int end = -5;
+
+        Period period = new Period(start, end);
+
+        assertEquals(period.duration(), -7);
+    }
+
+    @Test
+    void invalidOrder() {
+        int end = 6;
+        int start = 2;
+
+        Period period = new Period(end, start);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            period.duration();
+        });
+
+        assertEquals(-4, e.getMessage());
+    }
+    // end of test's for Period constructor
+
+
+
+
+    // test's for duration method
+    @Test
+    void validStartAndEndPeriod() {
+        int start = 4;
+        int end = 6;
+
+        Period p1 = new Period(start, end);
+
+        assertEquals(p1.duration(), 2);
+    }
+
+    @Test
     void invalidPeriod() {
         Period p1 = new Period(-1, 0);
+
         assertEquals(p1.duration(), -1);
     }
 
@@ -149,4 +174,75 @@ class MainTest {
 
     // end of test's for Overlaps(Period period) method
 
+
+
+
+
+
+    // below are tests for Rate constructor
+    @Test
+    void validRateObject() {
+
+        String kind = "";
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        BigDecimal normalRate = BigDecimal.valueOf(0.5);
+        BigDecimal reducedRate = BigDecimal.valueOf(0.5);
+
+        Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        assertNotNull(rate);
+    }
+
+
+    @Test
+    void invalidRateObject() {
+        String kind = "";
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        BigDecimal normalRate = BigDecimal.valueOf(0.5);
+        BigDecimal reducedRate = BigDecimal.valueOf(0.5);
+
+        Rate rate = null;
+        assertNotNull(rate);
+    }
+
+
+
+    // below are tests for calculate method
+    @Test
+    void validCalculation() {
+        int start = 5;
+        int end = 9;
+        Period period = new Period(start, end);
+
+
+        String kind = "";
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        BigDecimal normalRate = BigDecimal.valueOf(0.5);
+        BigDecimal reducedRate = BigDecimal.valueOf(0.5);
+
+        Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+
+        assertEquals(rate.calculate(period), rate);
+    }
+
+
+    @Test
+    void invalidCalculation() {
+        int start = 2;
+        int end = 4;
+        Period period = new Period(start, end);
+
+
+        String kind = "";
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        BigDecimal normalRate = BigDecimal.valueOf(0.5);
+        BigDecimal reducedRate = BigDecimal.valueOf(0.5);
+
+        Rate rate = null;
+
+        assertNotNull(rate.calculate(period));
+    }
 }
