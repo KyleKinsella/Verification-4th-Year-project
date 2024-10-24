b59d13f8f398cafeeee6b6698d7a2de3828c2675
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-24 13:01:14 +0100

    I have added some more tests to my test suite for the Rate Constructor

--- Task1/KinsellaKyleRateTests.java
+++ Task1/KinsellaKyleRateTests.java
@@ -3,12 +3,11 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class KinsellaKyleRateTests {
 
-    // below are tests for Rate constructor
+    // below are valid tests for the Rate constructor
     @Test
     void validRateObject() {
 
@@ -131,13 +130,128 @@ public class KinsellaKyleRateTests {
         Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
         assertEquals(normalPeriods, normalPeriods);
     }
+    // end of valid tests for the Rate constructor
 
 
 
+    // below are invalid tests for the Rate constructor
+    @Test
+    void invalidNormalRate() {
+        String kind = "Visitor";
+
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("-1");
+        BigDecimal reducedRate = new BigDecimal("-0.5");
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        });
+
+        assertEquals(e.getMessage(), normalRate);
+    }
 
 
+    @Test
+    void invalidReducedRate() {
+        String kind = "Visitor";
 
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
 
+        BigDecimal normalRate = new BigDecimal("0");
+        BigDecimal reducedRate = new BigDecimal("0");
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        });
+
+        assertEquals(e.getMessage(), reducedRate);
+    }
+
+    @Test
+    void invalidReducedRateArrayList() {
+        String kind = "Student";
+
+        Period period1 = new Period(-1, -2);
+        Period period2 = new Period(-42, -50);
+        Period period3 = new Period(-90, 55);
+
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        reducedPeriods.add(period1);
+        reducedPeriods.add(period2);
+        reducedPeriods.add(period3);
+
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("10");
+        BigDecimal reducedRate = new BigDecimal("5");
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        });
+
+        assertEquals(e.getMessage(), reducedRate);
+    }
+
+    @Test
+    void invalidNormalRateArrayList() {
+        String kind = "Student";
+
+        Period period1 = new Period(-1, 67);
+        Period period2 = new Period(90, -90);
+        Period period3 = new Period(65, -87);
+
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        normalPeriods.add(period1);
+        normalPeriods.add(period2);
+        normalPeriods.add(period3);
+
+        BigDecimal normalRate = new BigDecimal("10");
+        BigDecimal reducedRate = new BigDecimal("5");
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        });
+
+        assertEquals(e.getMessage(), normalPeriods);
+    }
+
+    @Test
+    void invalidCarParkKind() {
+        String kind = "Animals";
+
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("20");
+        BigDecimal reducedRate = new BigDecimal("10");
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        });
+
+        assertEquals(e.getMessage(), kind);
+    }
+
+    @Test
+    void invalidReducedRateTwo() {
+        String kind = "Staff";
+
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("5");
+        BigDecimal reducedRate = new BigDecimal("-90");
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        });
+
+        assertEquals(e.getMessage(), reducedRate);
+    }
 
 
 
