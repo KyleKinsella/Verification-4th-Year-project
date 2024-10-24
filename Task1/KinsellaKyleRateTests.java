8ea7b87e1bcb77adc3912fc4f7c2b289bccf1770
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-24 12:28:59 +0100

    I added more junit tests

--- Task1/KinsellaKyleRateTests.java
+++ Task1/KinsellaKyleRateTests.java
@@ -34,6 +34,124 @@ public class KinsellaKyleRateTests {
         assertNotNull(rate);
     }
 
+
+    @Test
+    void validNormalRate() {
+        String kind = "Student";
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("10");
+        BigDecimal reducedRate = new BigDecimal("5");
+
+        int start = 5;
+        int end = 10;
+
+        Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        Period period = new Period(start, end);
+
+        assertEquals("10", rate.calculate(period));
+    }
+
+
+    @Test
+    void validReducedRate() {
+        String kind = "Staff";
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("5");
+        BigDecimal reducedRate = new BigDecimal("2.5");
+
+        int start = 5;
+        int end = 10;
+
+        Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        Period period = new Period(start, end);
+
+        assertEquals("2.5", rate.calculate(period));
+    }
+
+    @Test
+    void validCarParkKind() {
+        String kind = "Management";
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("3");
+        BigDecimal reducedRate = new BigDecimal("1.5");
+
+        int start = 5;
+        int end = 10;
+
+        Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        Period period = new Period(start, end);
+
+        assertEquals(true, kind);
+    }
+
+    @Test
+    void validReducedRateArrayList() {
+        String kind = "Student";
+
+        Period period1 = new Period(1, 3);
+        Period period2 = new Period(2, 4);
+        Period period3 = new Period(5, 10);
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        reducedPeriods.add(period1);
+        reducedPeriods.add(period2);
+        reducedPeriods.add(period3);
+
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+
+        BigDecimal normalRate = new BigDecimal("10");
+        BigDecimal reducedRate = new BigDecimal("5");
+
+        Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        assertEquals(reducedPeriods, reducedPeriods);
+    }
+
+    @Test
+    void validNormalRateArrayList() {
+        String kind = "Student";
+
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+
+        Period period1 = new Period(5, 10);
+        Period period2 = new Period(4, 8);
+        Period period3 = new Period(1,10);
+        ArrayList<Period> normalPeriods = new ArrayList<>();
+        normalPeriods.add(period1);
+        normalPeriods.add(period2);
+        normalPeriods.add(period3);
+
+        BigDecimal normalRate = new BigDecimal("10");
+        BigDecimal reducedRate = new BigDecimal("5");
+
+        Rate rate = new Rate(kind, reducedPeriods, normalPeriods, normalRate, reducedRate);
+        assertEquals(normalPeriods, normalPeriods);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // below are tests for calculate method
     @Test
     void validCalculation() {
