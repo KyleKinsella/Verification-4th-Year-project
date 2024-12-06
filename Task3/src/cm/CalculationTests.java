14a42fd464ad18cee24d1bc90ad4c9ea0bd05364
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-12-06 17:30:17 +0000

    Update CalculationTests.java
    I refactored my test visitor test name.
    
    I have added a new test for management.

--- Task3/src/cm/CalculationTests.java
+++ Task3/src/cm/CalculationTests.java
@@ -7,8 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculationTests {
 
     @Test
-    void testVisitorCal() {
+    void testVisitorCalculation() {
         VisitorCalculation visitorCal = new VisitorCalculation();
         assertEquals(2.50, visitorCal.calculate(15.00), 0.01);
+    }
+
+    @Test
+    void testStaffCalculation() {
+        StaffCalculation staffCal = new StaffCalculation();
+        assertEquals(16.00, staffCal.calculate(20.00), 0.01);
     }
 }
