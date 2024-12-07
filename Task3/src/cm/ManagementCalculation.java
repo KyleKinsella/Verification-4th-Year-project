e334d1575888a61b7e497d97147e49aa6a5af3c6
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-12-07 11:31:27 +0000

    doing tdd wrong, now I am going to fix this
    I have just realized that I was not doing tdd right. I wrote the code then I wrote my tests, I believe this is wrong.
    
    So I am deleting my code files, and I am going to write a test then the code to pass that test, this is the right way to do tdd.

--- Task3/src/cm/ManagementCalculation.java
+++
@@ -1,8 +0,0 @@
-package cm;
-
-public class ManagementCalculation implements RateStrategy {
-    @Override
-    public double calculate(double totalCost) {
-        return Math.max(totalCost, 4.00);
-    }
-}
--- Task3/src/cm/StaffCalculation.java
+++
@@ -1,8 +0,0 @@
-package cm;
-
-public class StaffCalculation implements RateStrategy {
-    @Override
-    public double calculate(double totalCost) {
-        return Math.min(totalCost, 16.00);
-    }
-}
