39c4a8297e5caab88b0fcc6fba84b6f1ef3bdde1
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-12-07 11:35:20 +0000

    Update CalculationTests.java
    I have removed the three other tests as I am working on the one test at the moment.
    
    My test for visitor fails due to me not having and code for it, now I will write the code needed to pass this test.

--- Task3/src/cm/CalculationTests.java
+++ Task3/src/cm/CalculationTests.java
@@ -11,22 +11,4 @@ public class CalculationTests {
         VisitorCalculation visitorCal = new VisitorCalculation();
         assertEquals(2.50, visitorCal.calculate(15.00), 0.01);
     }
-
-    @Test
-    void testStaffCalculation() {
-        StaffCalculation staffCal = new StaffCalculation();
-        assertEquals(16.00, staffCal.calculate(20.00), 0.01);
-    }
-
-    @Test
-    void testManagementCalculation() {
-        ManagementCalculation managementCal = new ManagementCalculation();
-        assertEquals(4.00, managementCal.calculate(3.00), 0.01);
-    }
-
-    @Test
-    void testStudentCalculation() {
-        StudentCalculation studentCal = new StudentCalculation();
-        assertEquals(8.88, studentCal.calculate(10.00), 0.01);
-    }
 }
