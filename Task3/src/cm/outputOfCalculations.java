18e65d3977b97efe66589cca5b638d557d837b49
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-12-07 12:09:58 +0000

    made main file for all 4 types of car park kinds
    I have made a main file that prints the value for visitor, staff, management and student.

---
+++ Task3/src/cm/outputOfCalculations.java
@@ -0,0 +1,17 @@
+package cm;
+
+public class outputOfCalculations {
+    public static void main(String[] args) {
+        VisitorCalculation visitor = new VisitorCalculation();
+        System.out.println("Visitor rate for 15.00 is: " + visitor.calculate(15.00));
+
+        StaffCalculation staff = new StaffCalculation();
+        System.out.println("Staff rate for 20.00 is: " + staff.calculate(20.00));
+
+        ManagementCalculation management = new ManagementCalculation();
+        System.out.println("Management rate for 3.00 is: " + management.calculate(3.00));
+
+        StudentCalculation student = new StudentCalculation();
+        System.out.println("Student rate for 5.00 is: " + student.calculate(5.00));
+    }
+}
